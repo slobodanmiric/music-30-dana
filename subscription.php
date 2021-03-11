160286a4c1606ca0575fe18b9e8f4c63ef94bcec
slobodanmiric08051977 <slobamiric@gmail.com>
2021-03-11 20:13:29 +0100

    subscription html

--- subscription.php
+++ subscription.php
@@ -1,5 +1,26 @@
 <?php include "partials/header.php"; ?>
 
+    <section class="subscription">
+        <article class="container">
+            <div>
+                <h2>Subscription from $15/month</h2>
+                <h4>Start a free trial now</h4>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla repellendus pariatur delectus nihil quibusdam veritatis quae officiis exercitationem ab maxime! Quaerat explicabo exercitationem ipsam odit sit omnis tempora illum quibusdam.</p>
+                <button>Start free trial now</button>
+            </div>
+            <div>
+                <ul>
+                    <li>Play any track</li>
+                    <li>Listen offline</li>
+                    <li>No add interruptions</li>
+                    <li>Unlimited skips</li>
+                    <li>High quality</li>
+                    <li>Shuffle play</li>
+                </ul>
+            </div>
+        </article>
+    </section>
+
     <section class="container concept">
         <article>
             <h2>Our Concept & artists</h2>
